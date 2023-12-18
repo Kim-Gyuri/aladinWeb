6b898cbfc5d99107021f111b2a075d7fbaa6dfd2
Kim-Gyuri <gyuring0203@gmail.com>
2023-12-19 01:05:29 +0900

    s3 설정 수정

--- aladin/src/main/java/webservice/aladin/config/S3Config.java
+++ aladin/src/main/java/webservice/aladin/config/S3Config.java
@@ -3,6 +3,7 @@ package webservice.aladin.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,15 +16,15 @@ public class S3Config {
 
     @Value("${cloud.aws.region.static}")
     private String region;
-   @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
+   //@Value("${cloud.aws.credentials.access-key}")
+   // private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
+   // @Value("${cloud.aws.credentials.secret-key}")
+   // private String secretKey;
     @Bean
     public AmazonS3Client amazonS3Client() {
 
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSCredentials credentials = DefaultAWSCredentialsProviderChain.getInstance().getCredentials(); //new BasicAWSCredentials(accessKey, secretKey);
 
         return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
